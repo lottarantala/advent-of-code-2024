0f6b4129f42a1b241bfd6da3d05454f359a816bb
Lotta Rantala <lottarantala20@gmail.com>
2024-12-03 16:24:09 +0200

    Day 1 part 2 done

--- day1/main.cpp
+++ day1/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <bits/stdc++.h>
 
 int calculate_distance(std::vector<int> list1, std::vector<int> list2)
 {
@@ -15,6 +16,17 @@ int calculate_distance(std::vector<int> list1, std::vector<int> list2)
         total_distance += distance;
     }
     return total_distance;
+}
+
+int calculate_similarity_score(std::vector<int> list1, std::vector<int> list2)
+{
+    int similarity_score = 0;
+    for(auto i = 0u; i < list1.size(); i++)
+    {
+        auto count_of_item = std::count(list2.begin(), list2.end(), list1[i]);
+        similarity_score += list1[i] * count_of_item;
+    }
+    return similarity_score;
 }
 
 int main()
@@ -42,8 +54,10 @@ int main()
     std::sort(list2.begin(), list2.end());
 
     int total_distance = calculate_distance(list1, list2);
+    int similarity_score = calculate_similarity_score(list1, list2);
 
     std::cout << "Total distance: " << total_distance << std::endl;
+    std::cout << "Similarity score: " << similarity_score << std::endl;
 
     file.close();
     return 0;
